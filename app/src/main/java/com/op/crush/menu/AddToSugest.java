e2c4b8d82d280b657aba298a46638933c6432c82
Mostafa <dj.mostafa27@gmail.com>
2020-09-18 12:38:03 +0430

    v3.2

--- app/src/main/java/com/op/crush/menu/AddToSugest.java
+++ app/src/main/java/com/op/crush/menu/AddToSugest.java
@@ -9,7 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.firebase.FirebaseApp;
+
 import com.op.crush.R;
 
 public class AddToSugest extends Fragment {
--- app/src/main/java/com/op/crush/menu/HomeBottomFragment.java
+++ app/src/main/java/com/op/crush/menu/HomeBottomFragment.java
@@ -3,6 +3,7 @@ package com.op.crush.menu;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +16,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.op.crush.MainMenu;
 import com.op.crush.MyTwitterApiClient;
 import com.op.crush.R;
@@ -37,14 +40,13 @@ import retrofit2.Response;
 public class HomeBottomFragment extends Fragment {
 
     MainMenu m;
-    ImageView profile , banner;
-    Button follower_num , following_num ;
+    ImageView profile, banner;
+    Button follower_num, following_num;
     SharedPreferences preferences;
     TwitterSession session;
 
 
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    private FirebaseFirestore firestore;
 
 
 
@@ -55,31 +57,35 @@ public class HomeBottomFragment extends Fragment {
         final View view = inflater.inflate(R.layout.home_fragment, container, false);
         preferences = view.getContext().getSharedPreferences("Courser", Context.MODE_PRIVATE);
         session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        firestore.setFirestoreSettings(settings);
 
-        banner  = view.findViewById(R.id.banner_profile);
+        banner = view.findViewById(R.id.banner_profile);
         profile = view.findViewById(R.id.profile_image);
         follower_num = view.findViewById(R.id.follower_num);
         following_num = view.findViewById(R.id.following_num);
 
-        user_info(session,view.getContext());
+        user_info(session, view.getContext());
 
-        Map<String,Object> map = new HashMap<>();
-        map.put("id1",555555);
-        map.put("id2",666666);
-        db.collection("crush").document("user").set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Log.i("firebase","saved");
-            }
-        }).addOnFailureListener(new OnFailureListener() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id1", 555555);
+        map.put("id2", 666666);
+        firestore.collection("cr").document().set(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i("firebase", "saved");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.i("firebase","not saved"+e.getMessage());
+                Log.i("firebase", "not saved" + e.getMessage());
 
             }
         });
-
-
 
 
         follower_num.setOnClickListener(new View.OnClickListener() {
@@ -94,7 +100,7 @@ public class HomeBottomFragment extends Fragment {
                 Fragment followerYouNotFollow = new FollowerYouNotFollow();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 //FragmentManager.beginTransaction().add(R.id.fragment_container, fragment2, "2").hide(fragment2).commit();
-                transaction.replace(R.id.fragment_container, followerYouNotFollow ); // give your fragment container id in first parameter
+                transaction.replace(R.id.fragment_container, followerYouNotFollow); // give your fragment container id in first parameter
                 transaction.addToBackStack(null);  // if written, this transaction will be added to backstack
                 transaction.commit();
 
@@ -106,7 +112,7 @@ public class HomeBottomFragment extends Fragment {
             public void onClick(View view) {
                 Fragment fragment = new FollowingNotFollowYou();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.fragment_container, fragment );
+                transaction.replace(R.id.fragment_container, fragment);
                 transaction.addToBackStack(null);
                 transaction.commit();
 
@@ -115,13 +121,13 @@ public class HomeBottomFragment extends Fragment {
 
 
         return view;
-       // return inflater.inflate(R.layout.home_fragment,container,false);
+        // return inflater.inflate(R.layout.home_fragment,container,false);
     }
 
-    public void user_info(TwitterSession session, final Context context){
+    public void user_info(TwitterSession session, final Context context) {
 
         MyTwitterApiClient myTwitterApiClient = new MyTwitterApiClient(session);
-        myTwitterApiClient.getCustomTwitterService().User(session.getUserId() , session.getUserName()).enqueue(new Callback<UserShow>() {
+        myTwitterApiClient.getCustomTwitterService().User(session.getUserId(), session.getUserName()).enqueue(new Callback<UserShow>() {
             @Override
             public void onResponse(Call<UserShow> call, Response<UserShow> response) {
 
@@ -130,13 +136,13 @@ public class HomeBottomFragment extends Fragment {
                     UserShow show = response.body();
                     /*Toast.makeText(HomeBottomFragment.this, ""+show.getProfile_name() + "\n"
                             +show.getProfile_image_url() + "\n" + show.getFollowers_count(), Toast.LENGTH_SHORT).show();*/
-                    int cf = show.getFollowers_count()+show.getFollowings_count();
-                    preferences.edit().putInt("CP",cf ).apply();
+                    int cf = show.getFollowers_count() + show.getFollowings_count();
+                    preferences.edit().putInt("CP", cf).apply();
 
                     String purl = show.getProfile_image_url();
                     String burl = show.getProfile_banner_url();
-                    follower_num.setText("Follower\n"+String.valueOf(show.getFollowers_count()));
-                    following_num.setText("Following\n"+String.valueOf(show.getFollowings_count()));
+                    follower_num.setText("Follower\n" + String.valueOf(show.getFollowers_count()));
+                    following_num.setText("Following\n" + String.valueOf(show.getFollowings_count()));
                     String url = geturlpic(purl);
 
 
@@ -157,10 +163,10 @@ public class HomeBottomFragment extends Fragment {
 
     }
 
-    public String geturlpic(String s){
+    public String geturlpic(String s) {
         char[] chars = s.toCharArray();
-        String url="";
-        for (int i = 0 ; i<chars.length-11;i++){
+        String url = "";
+        for (int i = 0; i < chars.length - 11; i++) {
             url += chars[i];
         }
 
