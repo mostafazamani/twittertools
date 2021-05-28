6c9ba1e982c24a933cb3ffc0a1dd94597243422c
Mostafa <dj.mostafa27@gmail.com>
2021-05-28 11:56:54 +0430

    v5.7

--- app/src/main/java/com/op/crush/menu/FollowerYouNotFollow.java
+++ app/src/main/java/com/op/crush/menu/FollowerYouNotFollow.java
@@ -81,6 +81,8 @@ public class FollowerYouNotFollow extends Fragment {
     SharedPreferences preferences;
     int j = 0;
     ProgressDialog dialogf;
+    private SharedPreferences preferences1;
+    boolean adl = false;
 
 
     @Nullable
@@ -113,6 +115,10 @@ public class FollowerYouNotFollow extends Fragment {
         dialogf.setCancelable(false);
 
         preferences = view.getContext().getSharedPreferences("Courser", Context.MODE_PRIVATE);
+        preferences1 = view.getContext().getSharedPreferences("Courser", Context.MODE_PRIVATE);
+        String a = preferences1.getString("ad", "true");
+        if (a.equals("true"))
+            adl = true;
 
         if (preferences.getInt("FollowerCount", 0) == 1 && preferences.getInt("FollowingCount", 0) == 1) {
             progressBar.setVisibility(View.INVISIBLE);
@@ -236,88 +242,106 @@ public class FollowerYouNotFollow extends Fragment {
             @Override
             public void onClick(View view) {
 
-                dialog.show();
-                mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
-                    @Override
-                    public void onRewardedVideoAdLoaded() {
-                        Log.i("ad", "loaded");
-                        dialog.dismiss();
-                        mRewardedVideoAd.show();
-                    }
-
-                    @Override
-                    public void onRewardedVideoAdOpened() {
-                        Log.i("ad", "open");
-                    }
-
-                    @Override
-                    public void onRewardedVideoStarted() {
-                        Log.i("ad", "start");
-                    }
-
-                    @Override
-                    public void onRewardedVideoAdClosed() {
-                        Log.i("ad", "vclose");
-
-                    }
-
-                    @Override
-                    public void onRewarded(RewardItem rewardItem) {
-                        Log.i("ad", "reward");
-                        dialogf.show();
-
-
-                        int r = followList.size();
-
-                        if (r > 15) {
-                            for (int q = 0; q <= 15; q++) {
-                                new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
-                            }
-                            dialogf.dismiss();
-                        } else if (r != 0) {
-                            for (int q = 0; q < r - 1; q++) {
-                                new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
-                            }
-                            dialogf.dismiss();
+                if (adl) {
+                    dialog.show();
+                    mRewardedVideoAd.setRewardedVideoAdListener(new RewardedVideoAdListener() {
+                        @Override
+                        public void onRewardedVideoAdLoaded() {
+                            Log.i("ad", "loaded");
+                            dialog.dismiss();
+                            mRewardedVideoAd.show();
                         }
 
-                    }
-
-                    @Override
-                    public void onRewardedVideoAdLeftApplication() {
-                        Log.i("ad", "left");
-                    }
-
-                    @Override
-                    public void onRewardedVideoAdFailedToLoad(int i) {
-                        Log.i("ad", "filed");
-                        dialogf.show();
-                        int r = followList.size();
-
-                        if (r > 15) {
-                            for (int q = 0; q <= 15; q++) {
-                                new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
-                            }
-                            dialogf.dismiss();
-                        } else if (r != 0) {
-                            for (int q = 0; q < r - 1; q++) {
-                                new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
-
-                            }
-                            dialogf.dismiss();
+                        @Override
+                        public void onRewardedVideoAdOpened() {
+                            Log.i("ad", "open");
                         }
 
+                        @Override
+                        public void onRewardedVideoStarted() {
+                            Log.i("ad", "start");
+                        }
+
+                        @Override
+                        public void onRewardedVideoAdClosed() {
+                            Log.i("ad", "vclose");
+
+                        }
+
+                        @Override
+                        public void onRewarded(RewardItem rewardItem) {
+                            Log.i("ad", "reward");
+                            dialogf.show();
+
+
+                            int r = followList.size();
+
+                            if (r > 15) {
+                                for (int q = 0; q <= 15; q++) {
+                                    new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
+                                }
+                                dialogf.dismiss();
+                            } else if (r != 0) {
+                                for (int q = 0; q < r - 1; q++) {
+                                    new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
+                                }
+                                dialogf.dismiss();
+                            }
+
+                        }
+
+                        @Override
+                        public void onRewardedVideoAdLeftApplication() {
+                            Log.i("ad", "left");
+                        }
+
+                        @Override
+                        public void onRewardedVideoAdFailedToLoad(int i) {
+                            Log.i("ad", "filed");
+                            dialog.dismiss();
+                            dialogf.show();
+                            int r = followList.size();
+
+                            if (r > 15) {
+                                for (int q = 0; q <= 15; q++) {
+                                    new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
+                                }
+                                dialogf.dismiss();
+                            } else if (r != 0) {
+                                for (int q = 0; q < r - 1; q++) {
+                                    new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
+
+                                }
+                                dialogf.dismiss();
+                            }
+
+                        }
+
+                        @Override
+                        public void onRewardedVideoCompleted() {
+                            Log.i("ad", "completed");
+                        }
+                    });
+
+                    loadRewardedVideoAd();
+
+                }else{
+                    dialogf.show();
+                    int r = followList.size();
+
+                    if (r > 15) {
+                        for (int q = 0; q <= 15; q++) {
+                            new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
+                        }
+                        dialogf.dismiss();
+                    } else if (r != 0) {
+                        for (int q = 0; q < r - 1; q++) {
+                            new AllFollow(session, ynfAdapter, getContext(), followList, followList.get(0).getId(), q).execute();
+
+                        }
+                        dialogf.dismiss();
                     }
-
-                    @Override
-                    public void onRewardedVideoCompleted() {
-                        Log.i("ad", "completed");
-                    }
-                });
-
-                loadRewardedVideoAd();
-
-
+                }
             }
         });
 
