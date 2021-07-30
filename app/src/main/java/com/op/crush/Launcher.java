1332019ee6e84ce50b247d30c7df876f31669bc3
Mostafa <dj.mostafa27@gmail.com>
2021-07-30 11:49:21 +0430

    v6.1

--- app/src/main/java/com/op/crush/Launcher.java
+++ app/src/main/java/com/op/crush/Launcher.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Launcher extends AppCompatActivity {
 
@@ -11,7 +12,6 @@ public class Launcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launcher_activity);
-/*
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -21,26 +21,26 @@ public class Launcher extends AppCompatActivity {
 
                 finish();
             }
-        },1300); */
+        },2500);
 
 
-        Thread myThread = new Thread()
-        {
-            @Override
-            public void run() {
-                try {
-                    sleep(800);
-                    Intent intent = new Intent(getApplicationContext(),MainMenu.class);
-
-
-                    startActivity(intent);
-                    //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-                    finish();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        myThread.start();
+//        Thread myThread = new Thread()
+//        {
+//            @Override
+//            public void run() {
+//                try {
+//                    sleep(800);
+//                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+//
+//
+//                    startActivity(intent);
+//                    //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+//                    finish();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//        myThread.start();
     }
 }
